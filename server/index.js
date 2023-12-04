09fe42dad95fa04d7446a44a0d307e736c360d34
Sergey Georgiev <sgrgv_art@mail.ru>
2023-12-04 22:02:17 +0300

    вызываем функцию config у dotenv

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-
-// app.listen(5000, () => {
-//   console.log("Server is starting");
-// });
+dotenv.config();
 
 async function start() {
   try {
