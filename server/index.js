c567db5eaae2d287c42f9d98de955996f3ea6234
Sergey Georgiev <sgrgv_art@mail.ru>
2023-12-04 22:09:26 +0300

    создаем переменны и вставляем в URL

--- server/index.js
+++ server/index.js
@@ -5,15 +5,21 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+//contants
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
 async function start() {
   try {
     await mongoose.connect(
-      `mongodb+srv://sgrgv:sgrgv@pizza.p4vvat0.mongodb.net/Pizza?retryWrites=true&w=majority` // если выходит ошибка о whitelist, то необходимо поменять во вкладке Network Access IP адресс на 0.0.0.0/0
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@pizza.p4vvat0.mongodb.net/${DB_NAME}?retryWrites=true&w=majority` // если выходит ошибка о whitelist, то необходимо поменять во вкладке Network Access IP адресс на 0.0.0.0/0
     );
 
-    app.listen(3002, () => {
+    app.listen(PORT, () => {
       console.log(
-        `Hello motherfucker, server was started on the PORT: ${3002}`
+        `Hello motherfucker, server was started on the PORT: ${PORT}`
       );
     });
   } catch (error) {
