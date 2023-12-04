5951ed9074516e501cf86a65df59bc0e146bd88b
Sergey Georgiev <sgrgv_art@mail.ru>
2023-12-04 22:00:24 +0300

    подключаемся к базе данных

--- server/index.js
+++ server/index.js
@@ -3,6 +3,24 @@ import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is starting");
-});
+// app.listen(5000, () => {
+//   console.log("Server is starting");
+// });
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://sgrgv:sgrgv@pizza.p4vvat0.mongodb.net/Pizza?retryWrites=true&w=majority` // если выходит ошибка о whitelist, то необходимо поменять во вкладке Network Access IP адресс на 0.0.0.0/0
+    );
+
+    app.listen(3002, () => {
+      console.log(
+        `Hello motherfucker, server was started on the PORT: ${3002}`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
